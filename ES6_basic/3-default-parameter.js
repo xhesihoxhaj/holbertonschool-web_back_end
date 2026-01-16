f8af87cc55e76b3057941994319fd578f8ca680a
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-01-16 16:36:52 +0100

    def

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,2 @@
-export default function getSumOfHoods(initialNumber, expansion1989=89, expansion2019=19) {
-  
-  return initialNumber + expansion1989 + expansion2019;
-}
+const getSumOfHoods = (initialNumber, expansion1989=89, expansion2019=19) =>  initialNumber + expansion1989 + expansion2019; 
+export default getSumOfHoods;
