06a99295da73cc74f880a0f95c32a77c969e64fb
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-01-19 17:10:06 +0100

    3-fix

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,5 +1,3 @@
-const getSumOfHoods = (initialNumber=34, 
-    expansion1989 = 89, 
-    expansion2019 = 19) => 
-         initialNumber + expansion1989 + expansion2019; 
-export default getSumOfHoods;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
