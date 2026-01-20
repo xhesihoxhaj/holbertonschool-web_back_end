a32d08410e4ba7f11a5b00fa4a2b9bc167624fe8
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-01-20 13:41:09 +0100

    fixes for 12-main

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,3 +1,7 @@
 export default function createReportObject(employeesList) {
-
+  const object = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments: () => (Object.keys(employeesList).length),
+  };
+  return object;
 }
--- ES6_basic/12-main.js
+++ ES6_basic/12-main.js
@@ -1,10 +1,10 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
+import createEmployeesObject from './11-createEmployeesObject';
+import createReportObject from './12-createReportObject';
 
 const employees = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createEmployeesObject('marketing', ['Sylvie'])
-};      
+  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+  ...createEmployeesObject('marketing', ['Sylvie']),
+};
 
 const report = createReportObject(employees);
 console.log(report.allEmployees);
