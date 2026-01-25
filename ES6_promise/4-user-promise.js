ab62969d577cf3ea191ca27acc6e2931f891876c
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-01-25 20:08:29 +0100

    4

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,5 +1,3 @@
-/eslint-disable/
-
-export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({firstName,lastName,});
+export default function signUpUser(firstName, lastName) { return Promise.resolve({firstName,lastName,});
 }
+
