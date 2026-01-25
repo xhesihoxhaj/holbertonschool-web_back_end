c2ff0216733be86332d75c155df4f63db9c94980
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-01-25 21:55:21 +0100

    1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,4 @@
-import ClassRoom from './0-classroom';
-export default function initializeRooms(){
-    return[
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(20),
-    ];
-      
-
-    
+import Classroom from "./0-classroom";
+export default function initializeRooms() {
+    return [new Classroom(19), new Classroom(20), new Classroom(34)];
 }
