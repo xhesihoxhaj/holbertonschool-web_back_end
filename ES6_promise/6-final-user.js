d5dc407ce377acb65f2b73633b0a3155713eecff
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-01-25 20:22:20 +0100

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,11 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-import sign
-
-export default function handleProfileSignup(firstName,lastName ,fileName ){
-    const promise =[sign]
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
+  return Promise.allSettled(promises)
+    .then((results) => results.map((result) => (result.status === 'fulfilled'
+      ? { status: result.status, value: result.value }
+      : { status: result.status, value: result.reason.toString() })))
+    .catch((error) => error);
 }
