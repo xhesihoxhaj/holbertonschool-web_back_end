7544a94c6fad9c0a8baa11197287db850f73afbd
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-01-25 20:05:21 +0100

    3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,13 @@
-import { uploadPhoto , createUSer } from "./utils.js"
-function handleProfileSignup(){
-    return Promise.all([uploadPhoto(),createUSer()]);
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((response) => {
+      console.log(
+        `${response[0].body} ${response[1].firstName} ${response[1].lastName}`,
+      );
+    })
+    .catch(() => console.log('Signup system offline'));
 }
+
+export default handleProfileSignup;
