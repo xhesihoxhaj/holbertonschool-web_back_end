676375fa97aa15864b668340b681918af7e699d7
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-02-28 20:53:42 +0100

    last commit

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,12 +1,15 @@
 -- Create a function and divides two numbers
-DELIMITER $
+DELIMITER $$
+
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
+DETERMINISTIC
 BEGIN
     IF b != 0 THEN
         RETURN a / b;
     ELSE
         RETURN 0;
     END IF;
-END;
-$
+END$$
+
+DELIMITER ;
