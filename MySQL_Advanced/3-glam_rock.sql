94e910426755c1821e78cc83f6cc91ffa5cee951
xhesjanaHoxhaj <xhesjanahoxhaj@2rmlab.net>
2026-02-28 21:13:04 +0100

    the last

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,10 @@
--- 3-main.sql
-SOURCE metal_bands.sql;
-SOURCE 3-glam_rock.sql;
+-- 3-glam_rock.sql
+-- List all bands with Glam rock as their main style
+-- Ranked by their longevity up to the year 2024
+
+SELECT
+    band_name,
+    COALESCE(split, 2024) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
